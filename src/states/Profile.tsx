3bf25a84fd2fa15228f96d06c656dec1da04b4c9
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-04-24 07:44:40 -0400

    Minor Changes

--- src/states/Profile.tsx
+++ src/states/Profile.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Profile = (): JSX.Element => {
-  return <main>Welcome</main>;
+ return <main className="mt-20 text-white">Welcome</main>;
 };
 
 export default Profile;
