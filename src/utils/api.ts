a78b054a6a7dc29acd27bcfb4cf9b706f513a733
RyanLarge13 <ryanlarge13@gmail.com>
2024-04-29 22:32:04 -0400

    Creating Login Flow

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,56 +2,78 @@ import Axios, { AxiosResponse } from "axios";
 const devUrl = "http://localhost:8080";
 
 export const signUp = (newUser: {
- username: string;
- email: string;
- phone: string;
- password: string;
+  username: string;
+  email: string;
+  phone: string;
+  password: string;
 }): Promise<AxiosResponse> => {
- const res = Axios.post(`${devUrl}/signup`, { newUser });
- return res;
+  const res = Axios.post(`${devUrl}/signup`, { newUser });
+  return res;
 };
 
 export const verifyPhone = (
- token: string,
- pin: string
+  token: string,
+  pin: string
 ): Promise<AxiosResponse> => {
- const res = Axios.post(
-  `${devUrl}/verify/phone`,
-  { pin },
-  {
-   headers: {
-    Authorization: `Bearer ${token}`
-   }
-  }
- );
- return res;
+  const res = Axios.post(
+    `${devUrl}/verify/phone`,
+    { pin },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  return res;
 };
 
 export const verifyEmail = (
- token: string,
- pin: string
+  token: string,
+  pin: string
 ): Promise<AxiosResponse> => {
- const res = Axios.post(
-  `${devUrl}/verify/email`,
-  { pin },
-  { headers: { Authorization: `Bearer ${token} ` } }
- );
- return res;
+  const res = Axios.post(
+    `${devUrl}/verify/email`,
+    { pin },
+    { headers: { Authorization: `Bearer ${token} ` } }
+  );
+  return res;
+};
+
+export const verifyPhoneLogin = (
+  email: string,
+  pin: string
+): Promise<AxiosResponse> => {
+  const res = Axios.post(`${devUrl}/verify/pin/phone`, {
+    email: email,
+    pin: pin,
+  });
+  return res;
+};
+
+export const verifyEmailLogin = (
+  email: string,
+  pin: string
+): Promise<AxiosResponse> => {
+  const res = Axios.post(`${devUrl}/verify/pin/email`, {
+    email: email,
+    pin: pin,
+  });
+  return res;
 };
 
 export const fetchUserData = (token: string): Promise<AxiosResponse> => {
- const res = Axios.get(`${devUrl}/user`, {
-  headers: { Authorization: `Bearer ${token}` }
- });
- return res;
+  const res = Axios.get(`${devUrl}/user`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res;
 };
 
 export const pinEmail = (email: string): Promise<AxiosResponse> => {
- const res = Axios.post(`${devUrl}/verify/email/newpin`, { email: email });
- return res;
+  const res = Axios.post(`${devUrl}/verify/email/newpin`, { email: email });
+  return res;
 };
 
 export const pinPhone = (phone: string): Promise<AxiosResponse> => {
- const res = Axios.post(`${devUrl}/verify/phone/newpin`, { phone });
- return res;
+  const res = Axios.post(`${devUrl}/verify/phone/newpin`, { phone });
+  return res;
 };
