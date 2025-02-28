dc9a06efd13aaf0d8eb7afa831ca72bd9003dcdc
= <ryanhudsonlarge13@gmail.com>
2025-02-28 11:58:02 -0500

    Adding License To All Files

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,14 +1,29 @@
-import React, { useContext } from "react";
+/*
+Txt Me - A web based messaging platform
+Copyright (C) 2025 Ryan Large
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { motion } from "framer-motion";
-import { useNavigate } from "react-router-dom";
-import { getInitials } from "../utils/helpers";
-import UserCtxt from "../context/userCtxt";
+import React, { useContext } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import {
-  AllMessages,
-  Contacts as ContactsType,
-  Message,
-} from "../types/userTypes";
+import { useNavigate } from "react-router-dom";
+
+import UserCtxt from "../context/userCtxt";
+import { AllMessages, Contacts as ContactsType } from "../types/userTypes";
+import { getInitials } from "../utils/helpers";
 
 const Contacts = () => {
   const { contacts, setMessageSession, setAllMessages, allMessages } =
--- src/states/Profile.tsx
+++ src/states/Profile.tsx
@@ -1,10 +1,29 @@
+/*
+Txt Me - A web based messaging platform
+Copyright (C) 2025 Ryan Large
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import React, { useContext } from "react";
 import { TiMessages } from "react-icons/ti";
 import { Outlet } from "react-router-dom";
-import UserCtxt from "../context/userCtxt.tsx";
-import ProfileNav from "../components/ProfileNav.tsx";
+
 import BottomNav from "../components/BottomNav.tsx";
 import MessageSession from "../components/MessageSession.tsx";
+import ProfileNav from "../components/ProfileNav.tsx";
+import UserCtxt from "../context/userCtxt.tsx";
 
 const Profile = (): JSX.Element => {
   const { messageSession } = useContext(UserCtxt);
